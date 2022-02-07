05823201f6202a2cf037a5b1792f7fc046913213
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 10:35:29 +0300

    category configuration for ef implemented

---
+++ NLayerNet6.Repository/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerNet6.Core;
+
+namespace NLayerNet6.Repository.Configurations
+{
+    internal class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
