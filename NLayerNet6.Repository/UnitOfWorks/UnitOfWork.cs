230502b8a1146d42b10f4b51d2ed170f22e8fda9
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 12:24:22 +0300

    unit of work implemented

---
+++ NLayerNet6.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NLayerNet6.Core.UnitOfWorks;
+
+namespace NLayerNet6.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
