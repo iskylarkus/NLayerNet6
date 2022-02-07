11de8577a8dec6d5bea9ec2dec40e08a14f7c69a
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 14:34:32 +0300

    product feature dto created

---
+++ NLayerNet6.Core/Dtos/ProductFeatureDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NLayerNet6.Core.Dtos
+{
+    public class ProductFeatureDto
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int ProductId { get; set; }
+    }
+}
