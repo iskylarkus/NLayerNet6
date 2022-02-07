5564863058da031d47a25691373ecaadc3293d54
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 10:21:37 +0300

    dbcontext class generated

---
+++ NLayerNet6.Repository/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerNet6.Core;
+
+namespace NLayerNet6.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
