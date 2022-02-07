fb1433f25a11c6b7f375821b5a2e4718c84cfaf6
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 09:03:09 +0300

    core layer developed

---
+++ NLayerNet6.Core/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace NLayerNet6.Core
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
