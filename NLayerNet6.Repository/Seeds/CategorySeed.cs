754f6bbe280eca7adfb7b7ebcfae23dbc451635c
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 11:44:17 +0300

    category seed for initial data implemented

---
+++ NLayerNet6.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerNet6.Core;
+
+namespace NLayerNet6.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Kalemler"},
+                new Category { Id = 2, Name = "Kitaplar" },
+                new Category { Id = 3, Name = "Defterler" }
+            );
+        }
+    }
+}
