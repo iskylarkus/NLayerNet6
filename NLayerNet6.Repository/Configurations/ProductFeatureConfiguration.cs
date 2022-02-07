0668f03a00385333599d7465f23825cbffb38a6e
iskylarkus <turgay.kibar@gmail.com>
2022-02-07 10:44:02 +0300

    product feature configuration for ef implemented

---
+++ NLayerNet6.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerNet6.Core;
+
+namespace NLayerNet6.Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.ToTable("ProductFeatures");
+
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
