3abb153c86e21a24b8d3d468f3c6c2fe2a463749
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 12:51:43 +0300

    custom category repository with inheriting generic repository implemented

---
+++ NLayerNet6.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using NLayerNet6.Core.Models;
+
+namespace NLayerNet6.Core.Repositories
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        Task<Category> GetCategoryByIdWithProductsAsync(int id);
+    }
+}
---
+++ NLayerNet6.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerNet6.Core.Models;
+using NLayerNet6.Core.Repositories;
+
+namespace NLayerNet6.Repository.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+
+        public async Task<Category> GetCategoryByIdWithProductsAsync(int id)
+        {
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == id).SingleOrDefaultAsync();
+        }
+    }
+}
