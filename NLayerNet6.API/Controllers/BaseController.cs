4ea868b62300eb39b5735a6193ab2323685a9275
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 09:30:27 +0300

    base controller for common response object created

---
+++ NLayerNet6.API/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayerNet6.Core.Dtos;
+
+namespace NLayerNet6.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(ResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            }
+
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
