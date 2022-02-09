c2d1f34b45b11ca7b2490cf75fc8e856a34a4674
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 14:42:45 +0300

    product validator with fluentvalidation created

---
+++ NLayerNet6.Service/Validations/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using NLayerNet6.Core.Dtos;
+
+namespace NLayerNet6.Service.Validations
+{
+    public class ProductValidator:AbstractValidator<ProductDto>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("{PropertyName} is required")
+                .NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(x => x.Price).InclusiveBetween(1, decimal.MaxValue).WithMessage("{PropertyName} must be greater 0");
+
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be choosen");
+        }
+    }
+}
