246276220b7ddc7cc2a912c90c48cc0c6912aafe
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 15:02:23 +0300

    filter attribute for fluent validating developed

---
+++ NLayerNet6.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerNet6.Core.Dtos;
+
+namespace NLayerNet6.API.Filters
+{
+    public class ValidateFilterAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+
+                context.Result = new BadRequestObjectResult(ResponseDto<NoContentDto>.Fail(400, errors));
+            }
+        }
+    }
+}
