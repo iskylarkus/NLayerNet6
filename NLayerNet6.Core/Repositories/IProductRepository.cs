67c5e6607cb08fbfd24951c4488b1eafa6ede0e3
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 10:53:41 +0300

    custom product repository with inheriting generic repository implemented

---
+++ NLayerNet6.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using NLayerNet6.Core.Models;
+
+namespace NLayerNet6.Core.Repositories
+{
+    public interface IProductRepository:IRepository<Product>
+    {
+        Task<List<Product>> GetProductWithCategory();
+    }
+}
---
+++ NLayerNet6.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerNet6.Core.Models;
+using NLayerNet6.Core.Repositories;
+
+namespace NLayerNet6.Repository.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+
+        public async Task<List<Product>> GetProductWithCategory()
+        {
+            return await _context.Products.Include(x => x.Category).ToListAsync();
+        }
+    }
+}
