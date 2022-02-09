3690f6421e79b4a8ab7c66bb96fec7ee2d12c374
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 15:42:15 +0300

    custom exception handler middleware developed

---
+++ NLayerNet6.API/Middlewares/CustomExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using NLayerNet6.Core.Dtos;
+using NLayerNet6.Service.Exceptions;
+using System.Text.Json;
+
+namespace NLayerNet6.API.Middlewares
+{
+    public static class CustomExceptionHandler
+    {
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        _ => 500
+                    };
+                    
+                    context.Response.StatusCode = statusCode;
+
+                    var response = ResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                });
+            });
+        }
+    }
+}
---
+++ NLayerNet6.Service/Exceptions/ClientSideException.cs
@@ -0,0 +1,10 @@
+﻿namespace NLayerNet6.Service.Exceptions
+{
+    public class ClientSideException:Exception
+    {
+        public ClientSideException(string message):base(message)
+        {
+
+        }
+    }
+}
