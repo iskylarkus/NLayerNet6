92736dbf99dea67b2422b3890e2d45e68d1f1812
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 13:27:31 +0300

    getcategorybyidwithproducts action at new category controller developed

---
+++ NLayerNet6.API/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayerNet6.Core.Services;
+
+namespace NLayerNet6.API.Controllers
+{
+    [Route("api/categories")]
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> GetCategoryByIdWithProducts(int id)
+        {
+            return CreateActionResult(await _categoryService.GetCategoryByIdWithProductsAsync(id));
+        }
+    }
+}
