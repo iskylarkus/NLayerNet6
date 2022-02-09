4eac979d14e01d543f0fa2c68ca814e87a8b70b8
iskylarkus <turgay.kibar@gmail.com>
2022-02-09 18:20:59 +0300

    custom notfound async action filter middleware developed

---
+++ NLayerNet6.API/Filters/NotFoundFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerNet6.Core.Dtos;
+using NLayerNet6.Core.Models;
+using NLayerNet6.Core.Services;
+
+namespace NLayerNet6.API.Filters
+{
+    public class NotFoundFilter<T> : IAsyncActionFilter where T : BaseEntity
+    {
+        private readonly IService<T> _service;
+
+        public NotFoundFilter(IService<T> service)
+        {
+            _service = service;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var value = context.ActionArguments.Values.FirstOrDefault();
+
+            if (value == null)
+            {
+                await next.Invoke();
+                return;
+            }
+
+            var id = (int)value;
+
+            var entity = await _service.AnyAsync(x => x.Id == id);
+
+            if (entity)
+            {
+                await next.Invoke();
+                return;
+            }
+
+            context.Result = new NotFoundObjectResult(ResponseDto<NoContentDto>.Fail(404, $"{typeof(T).Name}({id}) not found"));
+        }
+    }
+}
