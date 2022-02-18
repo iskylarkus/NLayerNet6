926f0e7881c8d549db8242b5ddf17bd480f31e6d
iskylarkus <turgay.kibar@gmail.com>
2022-02-18 10:55:32 +0300

    product api service developed

--- NLayerNet6.Web/Services/ProductApiService.cs
+++ NLayerNet6.Web/Services/ProductApiService.cs
@@ -1,4 +1,7 @@
-﻿namespace NLayerNet6.Web.Services
+﻿using NLayerNet6.Core.Dtos;
+using NLayerNet6.Core.Models;
+
+namespace NLayerNet6.Web.Services
 {
     public class ProductApiService
     {
@@ -8,5 +11,48 @@
         {
             _httpClient = httpClient;
         }
+
+        public async Task<List<ProductWithCategoryDto>> GetProductWithCategoryAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<ResponseDto<List<ProductWithCategoryDto>>>("products/GetProductWithCategory");
+            return response.Data;
+        }
+
+        public async Task<ProductDto> SaveAsync(ProductDto productDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("products", productDto);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var responseBody = await response.Content.ReadFromJsonAsync<ResponseDto<ProductDto>>();
+            
+            return responseBody.Data;
+        }
+
+        public async Task<bool> UpdateAsync(ProductDto productDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync("products", productDto);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"products/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<ResponseDto<ProductDto>>($"products/{id}");
+
+            return response.Data;
+        }
+
+
     }
 }
