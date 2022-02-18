5e121ff45deb53d73f9ed534e70d9b99507dd1e6
iskylarkus <turgay.kibar@gmail.com>
2022-02-18 11:57:32 +0300

    new get all method for all category developed

--- NLayerNet6.API/Controllers/CategoryController.cs
+++ NLayerNet6.API/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayerNet6.Core.Dtos;
 using NLayerNet6.Core.Services;
 
 namespace NLayerNet6.API.Controllers
@@ -7,10 +9,22 @@ namespace NLayerNet6.API.Controllers
     public class CategoryController : BaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            return CreateActionResult(ResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
         }
 
         [HttpGet("[action]/{id}")]
